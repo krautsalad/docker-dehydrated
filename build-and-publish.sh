cd0630225243b165de0c7fc33ce47b2f3e3f7857
Sebastian Helbig <sebastian.helbig@krautsalad.com>
2025-06-25 12:12:29 +0200

    minor build change

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -ex
 
-docker build --no-cache --progress=plain -t krautsalad/dehydrated:latest -f docker/Dockerfile .
+DEHYDRATED_VERSION=0.7.2
+
+docker build \
+    --build-arg DEHYDRATED_VERSION=${DEHYDRATED_VERSION} \
+    --no-cache --progress=plain -t krautsalad/dehydrated:latest -f docker/Dockerfile .
 docker push krautsalad/dehydrated:latest
 
 VERSION=$(git describe --tags "$(git rev-list --tags --max-count=1)")
