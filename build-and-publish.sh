51f3993b74f80849ac43b756faeb00fa3d634c2d
Sebastian Helbig <sebastian.helbig@krautsalad.com>
2026-02-26 01:25:23 +0100

    fix for container running with pids of host

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
-set -ex
+set -euxo pipefail
 
 DEHYDRATED_VERSION=0.7.2
 
-docker build \
-    --build-arg DEHYDRATED_VERSION=${DEHYDRATED_VERSION} \
-    --no-cache --progress=plain -t krautsalad/dehydrated:latest -f docker/Dockerfile .
-docker push krautsalad/dehydrated:latest
-
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 VERSION=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 
-docker tag krautsalad/dehydrated:latest krautsalad/dehydrated:${VERSION}
-docker push krautsalad/dehydrated:${VERSION}
+BUILD_CONTEXT="${SCRIPT_DIR}/"
+
+docker buildx build \
+--build-arg DEHYDRATED_VERSION=${DEHYDRATED_VERSION} \
+--no-cache \
+--platform linux/amd64,linux/arm64 \
+--progress=plain \
+-f "${SCRIPT_DIR}/docker/Dockerfile" \
+-t krautsalad/dehydrated:latest \
+-t krautsalad/dehydrated:${VERSION} \
+"${BUILD_CONTEXT}"
+
+until docker buildx build \
+    --build-arg DEHYDRATED_VERSION=${DEHYDRATED_VERSION} \
+    --platform linux/amd64,linux/arm64 \
+    --push \
+    -f "${SCRIPT_DIR}/docker/Dockerfile" \
+    -t krautsalad/dehydrated:latest \
+    -t krautsalad/dehydrated:${VERSION} \
+    "${BUILD_CONTEXT}"; do
+    echo "Retrying push for ${image}-dev…" ; sleep 2
+done
